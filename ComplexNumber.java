eafb62c66239f9d541055d7d8a94a8e1dc10f8d9
ganga-17 <74947627+ganga-17@users.noreply.github.com>
2021-05-03 20:50:22 +0530

    course outcome1 question 3,4(3/05/2021)

---
+++ ComplexNumber.java
@@ -0,0 +1,26 @@
+public class ComplexNumber{
+    
+    double real, img;
+    ComplexNumber(double r, double i){
+     this.real = r;
+     this.img = i;
+    }
+     
+    public static ComplexNumber sum(ComplexNumber c1, ComplexNumber c2)
+    {
+     //creating a temporary complex number to hold the sum of two numbers
+         ComplexNumber temp = new ComplexNumber(0, 0);
+ 
+         temp.real = c1.real + c2.real;
+         temp.img = c1.img + c2.img;
+         
+         //returning the output complex number
+         return temp;
+     }
+     public static void main(String args[]) {
+     ComplexNumber c1 = new ComplexNumber(7.1,4);
+     ComplexNumber c2 = new ComplexNumber(8.0,5);
+         ComplexNumber temp = sum(c1, c2);
+         System.out.printf("Sum is: "+ temp.real+" + "+ temp.img +"i");
+     }
+ }
