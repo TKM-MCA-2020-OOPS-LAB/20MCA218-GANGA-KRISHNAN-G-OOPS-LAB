f803dc66a5bc4ee3cf454f577d7cd26b689efc2a
ganga-17 <74947627+ganga-17@users.noreply.github.com>
2021-09-07 19:10:08 +0530

    course outcome 4

---
+++ CO4/CO4-2/Calc.java
@@ -0,0 +1,14 @@
+  
+package Arithmetic;
+import Arithmetic.Operations;
+public class Calc {
+	public static void main(String[] args) {
+		Operations obj=new Operations();
+		obj.input();
+		obj.addition();
+		obj.subtraction();
+		obj.multiplication();
+		obj.divison();
+		}
+
+}
